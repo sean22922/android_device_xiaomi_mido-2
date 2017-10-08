f9f13a3c2a4a01f017471ada53b2525f62cdfcd7
dadi11 <dadi.sikorski@gmail.com>
2017-10-08 16:49:06 +0200

    Xenonify
    Change-Id: I6a6e2bd11adbfd9fefeef084fd0149f2eb656b63

--- xenonhd.mk
+++ xenonhd.mk
@@ -17,12 +17,19 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := xenonhd_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+ROOT_METHOD=magisk
+
+PRODUCT_PACKAGES += \
+    AdAway \
+    KernelAdiutor \
+    MiXplorer
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys" \
