ab6795bdd2bd2024f306e5af179876e3d7d394c2
dadi11 <dadi.sikorski@gmail.com>
2017-10-24 01:32:26 +0200

    mido: remove magisk
    Change-Id: I4eb260021734cbe3718b99978fa5a3f9eecb523a

--- xenonhd.mk
+++ xenonhd.mk
@@ -26,8 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=dadi11
 
-ROOT_METHOD=magisk
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys" \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V8.5.4.0.NCFMIED release-keys"
